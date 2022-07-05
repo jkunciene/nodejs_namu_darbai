1f1640c512dcf3c44b2d2b0eb31e777b28769ca7
jkunciene <j.kunciene@gmail.com>
2022-07-05 11:45:42 +0300

    express-async-handler, register, login

--- app.js
+++ app.js
@@ -6,14 +6,21 @@ connectDB();
 
 const User = require('./models/User');
 
+
 const express = require('express');
 const app = express();
 app.use(express.json());
 
 //controllers
-const createUser = require('./controllers/userController');
+const {
+    registerUser,
+    loginUser
+} = require('./controllers/userController');
 
-app.post('/api/user',createUser );
+
+app.post('/api/user', registerUser);
+app.post('/api/user/login', loginUser);
+
 
 app.listen(process.env.PORT, ()=> {
     console.log(`Server is running on PORT ${process.env.PORT}`)
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,67 @@
 const User = require('../models/User');
+const asyncHandler = require('express-async-handler');
 
-// @desc create new user
-// @route POST /api/user
-// @access
-const createUser = async(req, res) => {
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
+// @desc Register new user
+// @route POST /api/users
+// @access PUBLIC
+
+const registerUser = asyncHandler( async (req, res) => {
+    const { name, email, password } = req.body;
+    if(!name || !email || !password){
+        res.status(400);
+        throw new Error('Please add all fields');
+    }
+    //check if user exists
+    const userExists = await User.findOne({email});
+    if(userExists){
+        res.status(400);
+        throw new Error('User already exists');
+    }
+   
+    //Create User
+    const user = await User.create({
+        name,
+        email,
+        password
     });
-    const result = await user.save();
-    res.send(result);
-}
+    if(user){
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            password: user.password
+        })
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }    
+});
 
-module.exports = createUser;
+// @desc Login a user
+// @route POST /api/users/login
+// @access PUBLIC
+const loginUser = asyncHandler( async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({email});
+
+    if(user && password){
+        res.json(
+            {
+                _id: user.id,
+                name: user.name,
+                email: user.email,
+                password: user.password
+            } 
+        )
+    } else {
+        res.status(400);
+        throw new Error('Invalid credentials');
+    }
+
+});
+
+module.exports = {
+    registerUser,
+    loginUser,
+}
